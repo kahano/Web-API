a43958201d18176e02f10c5fbc92c5306e82154d
Mohamed Ali <kahano243@gmail.com>
2023-11-23 14:04:42 +0100

    new project added

---
+++ src/main/java/com/project/custom_product/CustomProductApplication.java
@@ -0,0 +1,13 @@
+package com.project.custom_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/custom_product/CustomProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.custom_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
